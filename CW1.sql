81fc188f9bc43f2cfb7a1f3f5d5f46cd253bdf16
Jacob Whitwell <jacob.whitwell@students.plymouth.ac.uk>
2021-11-12 15:06:23 +0000

    Inserted player table and added dummy data

--- CW1.sql
+++ CW1.sql
@@ -35,15 +35,24 @@ CREATE TABLE CW1.Treasure (
     CONSTRAINT fk_treasure_cacheID FOREIGN KEY (GeocacheID) REFERENCES CW1.Geocache(GeocacheID)
 )
 
-CREATE TABLE CW1.Found (
+CREATE TABLE CW1.PlayerGeocache (
     PlayerID INT IDENTITY(1, 1) NOT NULL,
     GeocacheID INT NOT NULL,
 
-    CONSTRAINT pk_found PRIMARY KEY (PlayerID),
+    CONSTRAINT fk_found_playerID FOREIGN KEY (PlayerID) REFERENCES CW1.Player(PlayerID),
     CONSTRAINT fk_found_cacheID FOREIGN KEY (GeocacheID) REFERENCES CW1.Geocache(GeocacheID)
+)
+
+CREATE TABLE CW1.Player (
+    PlayerID INT IDENTITY(1, 1) NOT NULL,
+    PlayerName VARCHAR(20) NOT NULL,
+    
+    CONSTRAINT pk_playerID PRIMARY KEY (PlayerID)
 )
 
 INSERT INTO CW1.Geocache (GeocacheLocation, GeocacheDescription, GeocacheStatus, GeocacheType)
 VALUES (123, 'In the town of Plymouth there is a cache', 'In-Play', 'Traditional Geocache'),
 (234, 'Beside Tower Bridge there is a small cache, covered by a rock', 'In-Play', 'Traditional Geocache'),
 (1234, 'Inside the minigolf course there are many pirates and water hazards. Here you would find what you might be looking for...', 'Maintenance', 'Traditional Geocache')
+
+
