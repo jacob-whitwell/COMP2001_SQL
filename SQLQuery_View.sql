2d4a60b7201d45bac6ab8dda7a40e9b55e49469a
Jacob Whitwell <jacob.whitwell@students.plymouth.ac.uk>
2021-11-13 22:44:01 +0000

    Adjusted Hint View order

--- SQLQuery_View.sql
+++ SQLQuery_View.sql
@@ -1,6 +1,6 @@
 -- View: Obtain a view of the hints for a geocache.
 CREATE VIEW CW1.ShowHints AS
-SELECT CW1.Geocache.GeocacheID, CW1.Geocache.GeocacheName, CW1.Hint.GeocacheHint, CW1.Hint.ID
+SELECT CW1.Hint.ID, CW1.Geocache.GeocacheID, CW1.Geocache.GeocacheName, CW1.Hint.GeocacheHint
 FROM CW1.Geocache, CW1.Hint
 WHERE CW1.Geocache.GeocacheID = CW1.Hint.GeocacheID
 
